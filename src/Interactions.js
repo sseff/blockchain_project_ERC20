79d04b757eb261e07706a40b6f447c6a41dfbe73
Elif Akalın <elifnurakalin@gmail.com>
2023-01-15 21:52:12 +0300

    view functions implemented, abi updated

--- src/Interactions.js
+++ src/Interactions.js
@@ -4,7 +4,22 @@ import styles from './Wallet.module.css'
 
 const Interactions = (props) => {
     const [transferHash, setTransferHash] = useState(null);
+    const [isProjectFunded, setisProjectFunded] = useState(null);
+    const [projectNextPayment, setprojectNextPayment] = useState(null);
+    const [projectOwner, setprojectOwner] = useState(null);
+    const [projectHash, setprojectHash] = useState(null);
+    const [projectPaySchedule, setprojectPaySchedule] = useState(null);
+    const [projectPaymentAmounts, setprojectPaymentAmounts] = useState(null);
+    const [projectVoteDeadline, setprojectVoteDeadline] = useState(null);
     const [noOfProjectProposals, setnoOfProjectProposals] = useState(null);
+    const [noOfFundedProposals, setnoOfFundedProposals] = useState(null);
+    const [etherReceivedByProject, setetherReceivedByProject] = useState(null);
+    const [noOfSurveys, setnoOfSurveys] = useState(null);
+    const [surveyResults, setSurveyResults] = useState([null]);
+    const [surveyHash, setSurveyHash] = useState([null]);
+    const [surveyinfo, setSurveyinfo] = useState(null);
+    const [surveyowner, setSurveyowner] = useState(null);
+    const [memberNumber, setMemberNumber] = useState(null);
 
 
     const transferHandler = async (e) => {
@@ -14,13 +29,6 @@ const Interactions = (props) => {
 
         let txt = await props.contract.transfer(receiverAddress, transferAmount);
         setTransferHash(txt.hash);
-    }
-
-    const noOfProjectProposalsViewer = async (e) => {
-        e.preventDefault();
-        let countBigNumber = await props.contract.getNoOfProjectProposals();
-        let count = countBigNumber.toNumber();
-        setnoOfProjectProposals(count);
     }
 
     const submitProposalHandler = async (e) => {
@@ -49,26 +57,119 @@ const Interactions = (props) => {
     }
 
     const isProjectFundedViewer = async (e) => {
+        e.preventDefault();
+        let id = e.target.projectid.value;
+        id=parseInt(id);
+        let tx = await props.contract.getIsProjectFunded(id);
+        if(tx){
+            setisProjectFunded("Yes");
+        }
+        else{
+            setisProjectFunded("No");
+        }
     }
     const ProjectNextPaymentViewer = async (e) => {
+        e.preventDefault();
+        let id = e.target.projectid.value;
+        id=parseInt(id);
+        let tx = await props.contract.getProjectOwner(id);
+        if(tx == "0x0000000000000000000000000000000000000000"){
+            tx = "No such project!";
+        }
+        else{
+            tx = await props.contract.getProjectNextPayment(id);
+        }
+        setprojectNextPayment(tx);
     }
     const ProjectOwnerViewer = async (e) => {
+        e.preventDefault();
+        let id = e.target.projectid.value;
+        id=parseInt(id);
+        let tx = await props.contract.getProjectOwner(id);
+        if(tx == "0x0000000000000000000000000000000000000000"){
+            tx = "No such project!";
+        }
+        setprojectOwner(tx);
     }
     const ProjectInfoViewer = async (e) => {
+        e.preventDefault();
+        let id = e.target.projectid.value;
+        id=parseInt(id);
+        let tx = await props.contract.getProjectInfo(id);
+        setprojectHash(tx[0]);
+        let deadline = tx[1].toNumber();
+        setprojectVoteDeadline(deadline);
+        setprojectPaySchedule(tx[2].toString());
+        setprojectPaymentAmounts(tx[3].toString());
+    }
+    const noOfProjectProposalsViewer = async (e) => {
+        e.preventDefault();
+        let countBigNumber = await props.contract.getNoOfProjectProposals();
+        let count = countBigNumber.toNumber();
+        setnoOfProjectProposals(count);
     }
     const noOfFundedProposalsViewer = async (e) => {
+        e.preventDefault();
+        let countBigNumber = await props.contract.getNoOfFundedProjects();
+        let count = countBigNumber.toNumber();
+        setnoOfFundedProposals(count);
     }
     const etherReceivedByProjectViewer = async (e) => {
+        e.preventDefault();
+        let id = e.target.projectid.value;
+        id=parseInt(id);
+        let tx = await props.contract.getEtherReceivedByProject(id);
+        tx = tx.toNumber();
+        setetherReceivedByProject(tx);
     }
     const noOfSurveysViewer = async (e) => {
+        e.preventDefault();
+        let countBigNumber = await props.contract.getNoOfSurveys();
+        let count = countBigNumber.toNumber();
+        setnoOfSurveys(count);
     }
     const noOfMembersViewer = async (e) => {
+        e.preventDefault();
+        let memberNum = await props.contract.getNoOfMembers();
+        memberNum = memberNum.toNumber();
+        setMemberNumber(memberNum);
     }
     const SurveyOwnerViewer = async (e) => {
+        e.preventDefault();
+        let surveyId = e.target.surveyid.value;
+        surveyId = parseInt(surveyId);
+        let surveyOwn = await props.contract.getSurveyOwner(surveyId);
+        if(surveyOwn == "0x0000000000000000000000000000000000000000"){
+            surveyOwn = "No such survey!";
+        }
+        setSurveyowner(surveyOwn);
     }
     const SurveyInfoViewer = async (e) => {
+        e.preventDefault();
+        let surveyId = e.target.surveyid.value;
+        let tx = await props.contract.getSurveyInfo(surveyId);
+        let ipfshash = "IPFS Hash: " + tx[0];
+        let surveyDeadline = tx[1];
+        let numchoices = tx[2].toNumber();
+        let atmost = tx[3].toNumber();
+        let info = "Survey Deadline: " + surveyDeadline + "\nNumber of Choices: " + numchoices + "\nAt Most Choice: " + atmost;
+        setSurveyHash(ipfshash);
+        setSurveyinfo(info);
     }
     const SurveyResultsViewer = async (e) => {
+        e.preventDefault();
+        let surveyId = e.target.surveyid.value;
+        let surveyOwn = await props.contract.getSurveyOwner(surveyId);
+        if(surveyOwn == "0x0000000000000000000000000000000000000000"){
+            setSurveyResults("No such survey!");
+        }
+        else{
+            let tx = await props.contract.getSurveyResults(surveyId);
+            let numtaken = tx[0];
+            let results = tx[1];
+            let str = "Number of voters taken: " + numtaken + "\nResults: " + results;
+            setSurveyResults(str);
+        }
     }
 
     return (
@@ -120,54 +221,79 @@ const Interactions = (props) => {
                 <button type='submit' className={styles.button6}>Donate</button>
             </form>
 
+
+
+
+
             <h3>View Functions:</h3>
             <form onSubmit={isProjectFundedViewer}>
+                <a>See if a project is funded by giving its project id:</a>
+                <input type='number' id='projectid'/>
                 <button type='submit' className={styles.button6}>✔</button>
-                <a>  {noOfProjectProposals}</a>
+                <a>  {isProjectFunded}</a>
             </form>
             <form onSubmit={ProjectNextPaymentViewer}>
+                <a>Get project's next payment by giving its project id:</a>
+                <input type='number' id='projectid'/>
                 <button type='submit' className={styles.button6}>✔</button>
-                <a>  {noOfProjectProposals}</a>
+                <a>  {projectNextPayment}</a>
             </form>
             <form onSubmit={ProjectOwnerViewer}>
+                <a>Get project's owner by giving its project id:</a>
+                <input type='number' id='projectid'/>
                 <button type='submit' className={styles.button6}>✔</button>
-                <a>  {noOfProjectProposals}</a>
+                <a> <br></br> {projectOwner}</a>
             </form>
             <form onSubmit={ProjectInfoViewer}>
+                <a>Get project's info by giving its project id:</a>
+                <input type='number' id='projectid'/>
                 <button type='submit' className={styles.button6}>✔</button>
-                <a>  {noOfProjectProposals}</a>
+                <a> <br></br> IPFS Hash: {projectHash} <br></br> Voting Deadline: {projectVoteDeadline}</a>
+                <a> <br/> Payment Amounts: {projectPaymentAmounts} <br></br> Payment Schedule: {projectPaySchedule}</a>
             </form>
             <form onSubmit={noOfProjectProposalsViewer}>
+                <a>Get number of project proposals: </a>
                 <button type='submit' className={styles.button6}>✔</button>
                 <a>  {noOfProjectProposals}</a>
             </form>
             <form onSubmit={noOfFundedProposalsViewer}>
+                <a>Get number of funded proposals: </a>
                 <button type='submit' className={styles.button6}>✔</button>
-                <a>  {noOfProjectProposals}</a>
+                <a>  {noOfFundedProposals}</a>
             </form>
             <form onSubmit={etherReceivedByProjectViewer}>
+                <a>Get project's total amount of ethers received by giving its project id:</a>
+                <input type='number' id='projectid'/>
                 <button type='submit' className={styles.button6}>✔</button>
-                <a>  {noOfProjectProposals}</a>
+                <a>  {etherReceivedByProject}</a>
             </form>
             <form onSubmit={noOfSurveysViewer}>
+                <a>Get number of submitted surveys: </a>
                 <button type='submit' className={styles.button6}>✔</button>
-                <a>  {noOfProjectProposals}</a>
+                <a>  {noOfSurveys}</a>
             </form>
             <form onSubmit={noOfMembersViewer}>
-                <button type='submit' className={styles.button6}>✔</button>
-                <a>  {noOfProjectProposals}</a>
+                <a>Get number of members:</a>      
+                <button type='submit' className={styles.button6}>✔</button> 
+                <a>{memberNumber}</a>
             </form>
             <form onSubmit={SurveyOwnerViewer}>
-                <button type='submit' className={styles.button6}>✔</button>
-                <a>  {noOfProjectProposals}</a>
+                <a>Get survey owner by its id:</a>          
+                <input type='number' id='surveyid'/>
+                <button type='submit' className={styles.button6}>✔</button> 
+                <a><br></br> {surveyowner}</a>
             </form>
-            <form onSubmit={SurveyInfoViewer}>
-                <button type='submit' className={styles.button6}>✔</button>
-                <a>  {noOfProjectProposals}</a>
+            <form onSubmit={SurveyInfoViewer}> 
+                <a>Get survey information by its id:</a>          
+                <input type='number' id='surveyid'/>
+                <button type='submit' className={styles.button6}>✔</button> 
+                <a><br></br> {surveyHash}<br></br> {surveyinfo}</a>
             </form>
-            <form onSubmit={SurveyResultsViewer}>
-                <button type='submit' className={styles.button6}>✔</button>
-                <a>  {noOfProjectProposals}</a>
+            <form onSubmit={SurveyResultsViewer}>     
+                <a>Get survey results by its id</a>          
+                <input type='number' id='surveyid'/>
+                <button type='submit' className={styles.button6}>✔</button> 
+                <a><br></br>{surveyResults}</a>
             </form>
 
         </div>
